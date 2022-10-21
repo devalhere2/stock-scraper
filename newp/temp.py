95f1e1991e3a5979af8f9f381bdcf31b38d1302b
deval patel <deval22990@gmail.com>
2022-10-22 01:54:06 +0530

    second commit

--- newp/temp.py
+++ newp/temp.py
@@ -2,11 +2,8 @@ import csv
 import os
 
 def getlink(name):
-    my_dic = csv.DictReader(open('Equity.csv'))
+    my_dic = csv.DictReader(open('deval-stock/Equity.csv'))
     for i in my_dic:
         if i['Security Id'] == name:
             num = i['Security Code']
             return [f'https://api.bseindia.com/BseIndiaAPI/api/getScripHeaderData/w?Debtflag=&scripcode={num}&seriesid=',f'https://api.bseindia.com/BseIndiaAPI/api/StockTrading/w?flag=&quotetype=EQ&scripcode={num}']
-
-print(getlink('GRAPHITE'))
-print(os.getcwd())
